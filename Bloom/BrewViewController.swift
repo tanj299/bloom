24e91ef6213950d0620005b2ad5777cfe4fd9dd9
Jayson Tan <tanj299@gmail.com>
2020-04-15 12:21:01 -0400

    modified all folder

--- Bloom/BrewViewController.swift
+++ Bloom/BrewViewController.swift
@@ -4,7 +4,11 @@
 //
 //  Created by Jayson Tan on 4/9/20.
 //  Copyright © 2020 Jayson Tan. All rights reserved.
-//
+//  SN: Student Notes - Intended for learning and note taking; do not take into consideration when reviewing
+
+
+//  SN: To allow ScrollView to work, every field must be constrained or simulator won't work
+//  This applies to ScrollView and TextView
 
 import UIKit
 
