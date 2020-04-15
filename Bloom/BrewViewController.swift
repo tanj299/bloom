7c85efb9ecdec7011560e785ef9d73d3d54138b0
Jayson Tan <tanj299@gmail.com>
2020-04-15 15:17:46 -0400

    added bunch of IBActions to 'Brew' page

--- Bloom/BrewViewController.swift
+++ Bloom/BrewViewController.swift
@@ -8,7 +8,10 @@
 
 
 //  SN: To allow ScrollView to work, every field must be constrained or simulator won't work
-//  This applies to ScrollView and TextView
+//  SN: This applies to ScrollView and TextView
+//  SN: @IBAction: Work to happen when you use the element (ie. hit a button)
+//  SB: @IBOutlet: Manipulate the element (ie. change its title, rounds it corners, etc.)
+//
 
 import UIKit
 
@@ -22,7 +25,71 @@ class BrewViewController: UIViewController {
     @IBAction func brewTest() {
         print("You are in Brew Controller")
     }
+    
+// MARK: - Immersion
+    
+    @IBAction func aeroPress() {
+        print("AeroPress button")
+    }
+    
+    @IBAction func frenchPress() {
+        print("French Press button")
+    }
+    
+    @IBAction func coldBrew() {
+        print("Cold Brew button")
+    }
+    
+    
+// MARK: - Percolation
+    
+    @IBAction func harioV60() {
+        print("Hario V60 button")
+    }
+    
+    @IBAction func kalitaWave() {
+        print("Kalita Wave button")
+    }
+    
+    @IBAction func chemex() {
+        print("Chemex button")
+    }
+    
+    @IBAction func bodum() {
+        print("Bodum button")
+    }
 
-
+    @IBAction func manualPourOver() {
+        print("Manual Pour Over button")
+    }
+ 
+// MARK: - Siphon
+    
+    @IBAction func harioTechnica() {
+        print("Hario Technica button")
+    }
+    
+    @IBAction func yamaGlass() {
+        print("Yama Glass button")
+    }
+    
+    @IBAction func bodumSantos() {
+        print("Bodum Santos button")
+    }
+    
+// MARK: - Miscellaneous
+    
+    @IBAction func espresso() {
+        print("Espresso button")
+    }
+    
+    @IBAction func mokaPot() {
+        print("Moka Pot button")
+    }
+    
+    @IBAction func phin() {
+        print("Phin button")
+    }
+    
 }
 
