ab461b3748076d292bf2c7e5fcec6d77cf4f86c5
Jayson Tan <tanj299@gmail.com>
2020-04-28 18:10:14 -0400

    got location, can zoom in on location with MapView

--- Bloom/DiscoverViewController.swift
+++ Bloom/DiscoverViewController.swift
@@ -12,27 +12,87 @@ import CoreLocation
 import CoreGraphics
 import Foundation
 
+// NOTE: Location spoofing has been moved to `Features` in update
+// To spoof location, go to Simulator -> Features -> Location -> Apple (or whatever)
 
-class DiscoverViewController: UIViewController {
-
+class DiscoverViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // CLLocationManager is the object that gives GPS coordinates
+    // Instantiate CLLocationManager
+    let locationManager = CLLocationManager()
+    
+    @IBAction func getLocation() {
+        
+        // Authorization for permission
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        // Check current authorization status; if .notDetermined
+        // Means that the app has not asked for permission yet
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
+        // CLLocationManager does not give coordinates immediately
+        // Have to invoke the class method, startUpdatedLocation() first
+        locationManager.startUpdatingLocation()
+        
+        centerOnLocation()
+    }
+    
+    // MARK: - Map View
+    let regionRadius: Double = 1000
+    func centerOnLocation () {
+        guard let coordinate = locationManager.location?.coordinate else {return}
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerOnLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print("didFailWithError \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdatedLocations \(newLocation)")
+    }
+    
+    
+    // MARK: - Helper Methods
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(
+            title: "Location Services Disabled",
+            message: "Please enable location services for this app in Settings.",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        // UIAlertController takes in a UIAlertAction argument
+        // Read documentation of class for iOS 8.0 and above
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func discoverTest() {
-        print("You are in Discover Controller")
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
